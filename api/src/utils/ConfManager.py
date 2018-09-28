2b1b6f193734d293cfbeef06adc9dba5d223d384
jeremymaignan <jeremy.maignan@gmail.com>
2018-09-28 10:51:32 +0200

    move conf file

--- api/src/utils/ConfManager.py
+++ api/src/utils/ConfManager.py
@@ -7,6 +7,6 @@ def get_conf(key):
     try:
         return os.environ[key.upper()]
     except:
-        with open('config.yaml') as json_data_file:
+        with open('../config.yaml') as json_data_file:
             conf = yaml.load(json_data_file)
             return conf[key]
