81f2dabe7a42c1f29fec4e3eda59dc2b268e1d54
jeremymaignan <jeremy.maignan@gmail.com>
2018-10-27 15:49:47 +0200

    add apps' ranking

--- api/src/controllers/apps.py
+++ api/src/controllers/apps.py
@@ -1,5 +1,7 @@
 from flask import Flask, Response, Blueprint, request, send_file
 import json
+import logging
+from urllib import parse
 
 from utils.ConfManager import get_conf
 
@@ -14,52 +16,132 @@ def get_apps_picture(app_name):
 
 @get_apps_details_route.route("/apps",  methods=['GET'])
 def get_apps_details():
+    try:
+        qs = parse.parse_qs(request.query_string.decode())
+        os = qs["os"][0]
+    except:
+        return Response(status=400)
+    if os not in ["ios", "android"]:
+        return Response(status=400)
     hostname = get_conf("api_hostname")
+    if os == "android":
+        return Response(
+            response=json.dumps({
+                "uber": {
+                    "picture_link": "{}/apps/pictures/uber".format(hostname),
+                    "deeplink": "",
+                    "rating": 4.2
+                },
+                "marcel": {
+                    "picture_link": "{}/apps/pictures/marcel".format(hostname),
+                    "deeplink": "",
+                    "rating": 3.9
+                },
+                "snapcar": {
+                    "picture_link": "{}/apps/pictures/snapcar".format(hostname),
+                    "deeplink": "",
+                    "rating": 3.4
+                },
+                "allocab": {
+                    "picture_link": "{}/apps/pictures/allocab".format(hostname),
+                    "deeplink": "",
+                    "rating": 3.9
+                },
+                "g7": {
+                    "picture_link": "{}/apps/pictures/g7".format(hostname),
+                    "deeplink": "",
+                    "rating": 3.7
+                },
+                "drive": {
+                    "picture_link": "{}/apps/pictures/drive".format(hostname),
+                    "deeplink": "",
+                    "rating": None
+                },
+                "citybird": {
+                    "picture_link": "{}/apps/pictures/citybird".format(hostname),
+                    "deeplink": "",
+                    "rating": 4.3
+                },
+                "felix": {
+                    "picture_link": "{}/apps/pictures/felix".format(hostname),
+                    "deeplink": "",
+                    "rating": 3.2
+                },
+                "lecab": {
+                    "picture_link": "{}/apps/pictures/lecab".format(hostname),
+                    "deeplink": "",
+                    "rating": 3.7
+                },
+                "taxify": {
+                    "picture_link": "{}/apps/pictures/taxify".format(hostname),
+                    "deeplink": "",
+                    "rating": 4.2
+                },
+                "talixo": {
+                    "picture_link": "{}/apps/pictures/talixo".format(hostname),
+                    "deeplink": "",
+                    "rating": 3.9
+                }
+            }),
+            status=200,
+            mimetype='application/json'
+        )
     return Response(
         response=json.dumps({
             "uber": {
                 "picture_link": "{}/apps/pictures/uber".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 4.2
             },
             "marcel": {
                 "picture_link": "{}/apps/pictures/marcel".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 3.1
             },
             "snapcar": {
                 "picture_link": "{}/apps/pictures/snapcar".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 3.8
             },
             "allocab": {
                 "picture_link": "{}/apps/pictures/allocab".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 4.7
             },
             "g7": {
                 "picture_link": "{}/apps/pictures/g7".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 3.9
             },
             "drive": {
                 "picture_link": "{}/apps/pictures/drive".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 4.9
             },
             "citybird": {
                 "picture_link": "{}/apps/pictures/citybird".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 4.4
             },
             "felix": {
                 "picture_link": "{}/apps/pictures/felix".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 3.5
             },
             "lecab": {
                 "picture_link": "{}/apps/pictures/lecab".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 3.7
             },
             "taxify": {
                 "picture_link": "{}/apps/pictures/taxify".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 4.8
             },
             "talixo": {
                 "picture_link": "{}/apps/pictures/talixo".format(hostname),
-                "deeplink": ""
+                "deeplink": "",
+                "rating": 4.6
             }
         }),
         status=200,
